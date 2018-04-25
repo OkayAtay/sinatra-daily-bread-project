63dd307d677e486e53b7f9fb02e82a8be01c2202
Araba Taylor <araba.taylor20@gmail.com>
2018-04-25 02:30:49 +0000

    Create Login and Signup Pages for Chefs

--- app/controllers/chefcontroller.rb
+++ app/controllers/chefcontroller.rb
@@ -4,4 +4,41 @@ class ChefController < ApplicationController
     erb :'/index'
   end
 
+  get '/signup' do
+    if logged_in?
+      redirect to '/recipes'
+    else
+      erb :'/chefs/signup'
+    end
+  end
+
+  post '/signup' do
+    if params[:username] == ""|| params[:email] == ""|| params[:password] == ""
+      redirect '/signup'
+    else
+      @chef = Chef.create(params)
+      @chef.save
+      session[:chef_id] = @chef.id
+      redirect '/recipe/recipes'
+    end
+  end
+
+  get '/login' do
+    if logged_in?
+      redirect '/recipe/recipes'
+    else
+      erb :'/chefs/login'
+    end
+  end
+
+  post '/login' do
+    @chef = Chef.find_by(username: params["username"])
+    if @chef && @chef.authenticate(params["password"])
+      session[:chef_id] = @chef.id
+      redirect '/recipes/recipes'
+    else
+      redirect '/chefs/login'
+    end
+  end
+
 end
