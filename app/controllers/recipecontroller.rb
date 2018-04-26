60deeacceb5ca46352ba21c4bde0976a0201aa7b
Araba Taylor <araba.taylor20@gmail.com>
2018-04-26 01:28:11 +0000

    Get, post, and structure for Create_recipe

--- app/controllers/recipecontroller.rb
+++ app/controllers/recipecontroller.rb
@@ -1,2 +1,22 @@
 class RecipeController < ApplicationController
+
+  get '/recipes' do
+    @recipes = Recipe.all
+  end
+
+  get '/recipes/new' do
+    erb :'/recipes/create_recipe'
+  end
+
+  post '/recipes/new' do
+    binding.pry
+    @recipe = Recipe.create(params)
+    # @recipe.user_id = current_user.id
+    @recipe.save
+  end
+
+  get '/recipes/:id/edit' do
+    @recipe = Recipe.find_by_id(params[:id])
+    erb :'/recipes/edit_recipe'
+  end
 end
